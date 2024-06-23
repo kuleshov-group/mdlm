4bba3904928000ba5480d3a2adaac6bfa8e80244
Volodymyr Kuleshov <vol.kuleshov@gmail.com>
2024-06-23 15:54:16 -0400

    move eval and metrics

---
+++ algorithms/core/metrics.py
@@ -0,0 +1,33 @@
+import math
+import torch
+import torchmetrics
+
+LOG2 = math.log(2)
+
+@dataclass
+class Loss:
+  loss: torch.FloatTensor
+  nlls: torch.FloatTensor
+  token_mask: torch.FloatTensor
+
+
+class NLL(torchmetrics.aggregation.MeanMetric):
+  pass
+
+class BPD(NLL):
+  def compute(self) -> Tensor:
+    """Computes the bits per dimension.
+
+    Returns:
+      bpd
+    """
+    return self.mean_value / self.weight / LOG2
+
+class Perplexity(NLL):
+  def compute(self) -> Tensor:
+    """Computes the Perplexity.
+
+    Returns:
+     Perplexity
+    """
+    return torch.exp(self.mean_value / self.weight)
