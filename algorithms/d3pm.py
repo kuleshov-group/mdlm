313684a6d5925bca00f7dc2026434dd70ce3e2a8
Volodymyr Kuleshov <vol.kuleshov@gmail.com>
2024-06-25 17:54:51 -0400

    factor out d3pm into its own class

---
+++ algorithms/d3pm.py
@@ -0,0 +1,150 @@
+import itertools
+import numpy as np
+import torch
+import transformers
+
+import models
+import noise_schedule
+import utils
+
+from .core.diffusion import CoreDiffusion
+from .core.lightning import LightningDiffusion
+from .core.sampling.ancestral import AncestralSampler
+from .core.genppl import GenPPLEvaluator
+
+
+class D3PM(
+  CoreDiffusion, LightningDiffusion, AncestralSampler, GenPPLEvaluator
+):
+  def __init__(
+    self,
+    config,
+    tokenizer: transformers.PreTrainedTokenizer
+  ):
+    CoreDiffusion.__init__(
+      self, 
+      config, 
+      mask_token_id=tokenizer.mask_token_id
+    )
+    LightningDiffusion.__init__(self, config, tokenizer)
+    AncestralSampler.__init__(self, config)
+    GenPPLEvaluator.__init__(self, config)
+    self.neg_infinity = -1000000.0
+
+  def _d3pm_parameterization(self, logits):
+    if self.subs_masking:
+      logits[:, :, self.mask_index] += self.neg_infinity
+    logits = logits - torch.logsumexp(logits, dim=-1,
+                                      keepdim=True)
+    return logits
+
+  def forward(self, x, sigma):
+    """Returns log score."""
+    sigma = self._process_sigma(sigma)
+    with torch.cuda.amp.autocast(dtype=torch.float32):
+      logits = self.backbone(x, sigma)
+    return self._d3pm_parameterization(logits=logits)
+
+  def _d3pm_loss(self, model_output, xt, x0, t):
+    dt = 1 / self.T
+
+    if torch.is_tensor(t):
+      t = t[:, None]
+      assert t.ndim == 2
+      t = t.clamp(0., 1. - 1e-4)
+    alpha_t = 1 - t + torch.zeros_like(xt)
+    alpha_s = 1 - (t - dt) + torch.zeros_like(xt)
+
+    log_x_theta_at_x0 = torch.gather(
+      model_output, -1, x0[:, :, None]).squeeze(-1)
+    log_x_theta_at_m = model_output[:, :, self.mask_index]
+    x_theta_at_m = log_x_theta_at_m.exp()
+    
+    term_1_coef = dt / t
+    term_1_log_nr = torch.log(alpha_t * x_theta_at_m / t + 1)
+    term_1_log_dr = log_x_theta_at_x0
+    
+    term_2_coef = 1 - dt / t
+    term_2_log_nr = term_1_log_nr
+    term_2_log_dr = torch.log(alpha_s * x_theta_at_m / (t - dt) + 1)
+
+    L_vb_masked = (
+      term_1_coef * (term_1_log_nr - term_1_log_dr)
+      + term_2_coef * (term_2_log_nr - term_2_log_dr))
+
+    L_vb = L_vb_masked * (xt == self.mask_index)
+
+    return self.T * L_vb
+
+  def _diffusion_elbo(self, x0):
+    t = self._sample_t(x0.shape[0], x0.device)
+    if self.T > 0:
+      t = (t * self.T).to(torch.int)
+      t = t / self.T
+      # t \in {1/T, 2/T, ..., 1}
+      t += (1 / self.T)
+
+    if self.change_of_variables:
+      unet_conditioning = t[:, None]
+      f_T = torch.log1p(- torch.exp(- self.noise.sigma_max))
+      f_0 = torch.log1p(- torch.exp(- self.noise.sigma_min))
+      move_chance = torch.exp(f_0 + t * (f_T - f_0))
+      move_chance = move_chance[:, None]
+    else:
+      sigma, dsigma = self.noise(t)
+      unet_conditioning = sigma[:, None]
+      move_chance = 1 - torch.exp(-sigma[:, None])
+
+    xt = self.q_xt(x0, move_chance)
+    model_output = self.forward(xt, unet_conditioning)
+    utils.print_nans(model_output, 'model_output')
+
+    if self.T > 0:
+      diffusion_loss = self._d3pm_loss(
+        model_output=model_output, xt=xt, x0=x0, t=t)
+      reconstruction_loss = self._reconstruction_loss(x0)
+      return reconstruction_loss + diffusion_loss
+    elif if self.T == 0:
+      # we are in continuous time    
+      raise NotImplementedError()
+
+    @torch.no_grad()
+  def _sample(self, num_steps=None, eps=1e-5):
+    """Generate samples from the model."""
+    batch_size_per_gpu = self.config.loader.eval_batch_size
+    # Lightning auto-casting is not working in this method for some reason
+    if num_steps is None:
+      num_steps = self.config.sampling.steps
+    x = self._sample_prior(
+      batch_size_per_gpu,
+      self.config.model.length).to(self.device)
+    timesteps = torch.linspace(
+      1, eps, num_steps + 1, device=self.device)
+    dt = (1 - eps) / num_steps
+    p_x0_cache = None
+
+    for i in range(num_steps):
+      t = timesteps[i] * torch.ones(
+        x.shape[0], 1, device=self.device)
+      if self.sampler == 'ddpm':
+        x = self._ddpm_update(x, t, dt)
+      elif self.sampler == 'ddpm_cache':
+        p_x0_cache, x_next = self._ddpm_caching_update(
+          x, t, dt, p_x0=p_x0_cache)
+        if (not torch.allclose(x_next, x)
+            or self.time_conditioning):
+          # Disable caching
+          p_x0_cache = None
+        x = x_next
+      else:
+        raise NotImplementedError()
+
+    if self.config.sampling.noise_removal:
+      t = timesteps[-1] * torch.ones(x.shape[0], 1,
+                                     device=self.device)
+      if self.sampler == 'analytic':
+        raise NotImplementedError()
+      else:
+        unet_conditioning = self.noise(t)[0]
+        x = self.forward(x, unet_conditioning).argmax(dim=-1)
+    return x
