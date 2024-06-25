012a39c4b005a607ac825d3ded86a517961391f5
Volodymyr Kuleshov <vol.kuleshov@gmail.com>
2024-06-24 23:36:19 -0400

    add lightning hooks class

---
+++ algorithms/core/lightning.py
@@ -0,0 +1,259 @@
+"""Implements hooks used by PyTorch Lightning"""
+import torch
+import hydra.utils
+import lightning as L
+import torchmetrics
+import dataloader
+from .metrics import NLL, BPD, Perplexity
+
+active_matrics = {
+  'nll': NLL(),
+  'bpd': BPD(),
+  'ppl': Perplexity(),
+}
+
+class LightningDiffusion(L.LightningModule):
+  def __init__(
+    self,
+    config,
+    tokenizer: transformers.PreTrainedTokenizer
+  ):
+    self.config = config
+    self.tokenizer = tokenizer
+
+    # metrics will automatically reset at end of epoch
+    metrics = torchmetrics.MetricCollection({
+      active_metrics
+    })
+    metrics.set_dtype(torch.float64)
+    self.train_metrics = metrics.clone(prefix='train/')
+    self.valid_metrics = metrics.clone(prefix='val/')
+    self.test_metrics = metrics.clone(prefix='test/')
+
+  # we want to implement the following abstract functions
+
+  def forward(self, x, sigma_t):
+    raise NotImplementedError()
+
+  def noise(self, x, sigma_t):
+    raise NotImplementedError()
+
+  def _loss(batch, attention_mask):
+    raise NotImplementedError()
+
+  def train_mode(self, ema):
+    raise NotImplementedError()
+
+  def eval_mode(self, ema):
+    raise NotImplementedError()
+
+  def store_ema(self):
+    raise NotImplementedError()
+
+  def restore_ema(self):
+    raise NotImplementedError()
+
+  def update_ema(self):
+    raise NotImplementedError()
+
+  def load_ema_from_checkpoint(self, checkpoint):
+    raise NotImplementedError()
+
+  def save_ema_to_checkpoint(self, checkpoint):
+    raise NotImplementedError()
+
+  def move_ema_shadow_params_to_device():
+    raise NotImplementedError()
+
+  def iter_params(self):
+    raise NotImplementedError
+
+  # FIXME: also requires compute_generative_perplexity to be def by subclass
+  # (better to make dependency more explicit)
+
+  def on_load_checkpoint(self, checkpoint):
+    self.load_ema_from_checkpoint()
+    # Copied from:
+    # https://github.com/Dao-AILab/flash-attention/blob/main/training/src/datamodules/language_modeling_hf.py#L41
+    self.fast_forward_epochs = checkpoint['loops'][
+      'fit_loop']['epoch_progress']['current']['completed']
+    self.fast_forward_batches = checkpoint['loops'][
+      'fit_loop']['epoch_loop.batch_progress'][
+        'current']['completed']
+
+  def on_save_checkpoint(self, checkpoint):
+    checkpoint = self.save_ema_to_checkpoint(checkpoint)
+    # Copied from:
+    # https://github.com/Dao-AILab/flash-attention/blob/main/training/src/tasks/seq.py
+    # ['epoch_loop.batch_progress']['total']['completed'] is 1 iteration
+    # behind, so we're using the optimizer's progress.
+    checkpoint['loops']['fit_loop'][
+      'epoch_loop.batch_progress']['total'][
+        'completed'] = checkpoint['loops']['fit_loop'][
+          'epoch_loop.automatic_optimization.optim_progress'][
+            'optimizer']['step']['total'][
+              'completed'] * self.trainer.accumulate_grad_batches
+    checkpoint['loops']['fit_loop'][
+      'epoch_loop.batch_progress']['current'][
+        'completed'] = checkpoint['loops']['fit_loop'][
+          'epoch_loop.automatic_optimization.optim_progress'][
+            'optimizer']['step']['current'][
+              'completed'] * self.trainer.accumulate_grad_batches
+    # _batches_that_stepped tracks the number of global steps, not the number
+    # of local steps, so we don't multiply with self.trainer.accumulate_grad_batches here.
+    checkpoint['loops']['fit_loop'][
+      'epoch_loop.state_dict'][
+        '_batches_that_stepped'] = checkpoint['loops']['fit_loop'][
+          'epoch_loop.automatic_optimization.optim_progress'][
+            'optimizer']['step']['total']['completed']
+    if 'sampler' not in checkpoint.keys():
+      checkpoint['sampler'] = {}
+    if hasattr(self.trainer.train_dataloader.sampler,
+               'state_dict'):
+      sampler_state_dict = self.trainer.\
+        train_dataloader.sampler.state_dict()
+      checkpoint['sampler'][
+        'random_state'] = sampler_state_dict.get(
+          'random_state', None)
+    else:
+      checkpoint['sampler']['random_state'] = None
+
+  def on_train_start(self):
+    self.move_ema_shadow_params_to_device()
+    # Adapted from:
+    # https://github.com/Dao-AILab/flash-attention/blob/main/training/src/datamodules/language_modeling_hf.py
+    distributed = (
+      self.trainer._accelerator_connector.use_distributed_sampler
+      and self.trainer._accelerator_connector.is_distributed)
+    if distributed:
+      sampler_cls = dataloader.FaultTolerantDistributedSampler
+    else:
+      sampler_cls = dataloader.RandomFaultTolerantSampler
+    updated_dls = []
+    for dl in self.trainer.fit_loop._combined_loader.flattened:
+      if hasattr(dl.sampler, 'shuffle'):
+        dl_sampler = sampler_cls(
+          dl.dataset, shuffle=dl.sampler.shuffle)
+      else:
+        dl_sampler = sampler_cls(dl.dataset)
+      if (distributed
+          and self.fast_forward_epochs is not None
+          and self.fast_forward_batches is not None):
+        dl_sampler.load_state_dict({
+          'epoch': self.fast_forward_epochs,
+          'counter': (self.fast_forward_batches
+                      * self.config.loader.batch_size)})
+      updated_dls.append(
+        torch.utils.data.DataLoader(
+          dl.dataset,
+          batch_size=self.config.loader.batch_size,
+          num_workers=self.config.loader.num_workers,
+          pin_memory=self.config.loader.pin_memory,
+          sampler=dl_sampler,
+          shuffle=False,
+          persistent_workers=True))
+    self.trainer.fit_loop._combined_loader.flattened = updated_dls
+
+  def optimizer_step(self, *args, **kwargs):
+    super().optimizer_step(*args, **kwargs)
+    self.update_ema()
+
+  def on_train_epoch_start(self):
+    self.train_mode(ema=False)
+
+  def training_step(self, batch, batch_idx):
+    loss = self._compute_loss(batch, prefix='train')
+    self.log(name='trainer/loss',
+             value=loss.item(),
+             on_step=True,
+             on_epoch=False,
+             sync_dist=True)
+    return loss
+
+  def on_validation_epoch_start(self):
+    self.eval_mode()
+    assert self.valid_metrics.nll.mean_value == 0
+    assert self.valid_metrics.nll.weight == 0
+
+  def validation_step(self, batch, batch_idx):
+    return self._compute_loss(batch, prefix='val')
+
+  def _compute_loss(self, batch, prefix):
+    if 'attention_mask' in batch:
+      attention_mask = batch['attention_mask']
+    else:
+      attention_mask = None
+    losses = self._loss(batch['input_ids'], attention_mask)
+    loss = losses.loss
+
+    if prefix == 'train':
+      self.train_metrics.update(losses.nlls, losses.token_mask)
+      metrics = self.train_metrics
+    elif prefix == 'val':
+      self.valid_metrics.update(losses.nlls, losses.token_mask)
+      metrics = self.valid_metrics
+    elif prefix == 'test':
+      self.test_metrics.update(losses.nlls, losses.token_mask)
+      metrics = self.test_metrics
+    else:
+      raise ValueError(f'Invalid prefix: {prefix}')
+
+    self.log_dict(metrics,
+                  on_step=False,
+                  on_epoch=True,
+                  sync_dist=True)
+    return loss
+
+  def on_validation_epoch_end(self):
+    if ((self.config.eval.compute_perplexity_on_sanity
+         or not self.trainer.sanity_checking)
+         and self.config.eval.generate_samples
+         and not self.config.parameterization == 'ar'):
+      # TODO(justin): implement sampling and kv cache for AR
+      samples, text_samples = None, None
+      for _ in range(
+        self.config.sampling.num_sample_batches):
+        samples = self._sample()
+        # Decode the samples to be re-tokenized by eval model
+        text_samples = self.tokenizer.batch_decode(samples)
+        if self.config.eval.compute_generative_perplexity:
+          self.compute_generative_perplexity(text_samples)
+      if self.trainer.global_rank == 0 and hasattr(
+        self.trainer.logger, 'log_table'):
+        # Log the last generated samples
+        text_samples = text_samples[
+          : self.config.sampling.num_sample_log]
+        self.trainer.logger.log_table(
+          key=f'samples@global_step{self.global_step}',
+          columns=['Generated Samples'],
+          data=[[s] for s in text_samples])
+      if self.config.eval.compute_generative_perplexity:
+        self.log('val/gen_ppl',
+                 self.gen_ppl_metric,
+                 on_epoch=True,
+                 on_step=False,
+                 sync_dist=True)
+    self.restore_ema()
+
+  def configure_optimizers(self):
+    # TODO(yair): Lightning currently giving this warning when using `fp16`:
+    #  "Detected call of `lr_scheduler.step()` before `optimizer.step()`. "
+    #  Not clear if this is a problem or not.
+    #  See: https://github.com/Lightning-AI/pytorch-lightning/issues/5558
+    optimizer = torch.optim.AdamW(
+      self.iter_params(),
+      lr=self.config.optim.lr,
+      betas=(self.config.optim.beta1,
+             self.config.optim.beta2),
+      eps=self.config.optim.eps,
+      weight_decay=self.config.optim.weight_decay)
+
+    scheduler = hydra.utils.instantiate(
+      self.config.lr_scheduler, optimizer=optimizer)
+    scheduler_dict = {
+      'scheduler': scheduler,
+      'interval': 'step',
+      'monitor': 'val/loss',
+      'name': 'trainer/lr',
+    }
+    return [optimizer], [scheduler_dict]
