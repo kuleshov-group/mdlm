357b32a7a5b91a659e3366dd7e9b56ac5b1c4869
Volodymyr Kuleshov <vol.kuleshov@gmail.com>
2024-06-24 23:35:46 -0400

    add sampling classes

---
+++ algorithms/core/sampling/analytic.py
@@ -0,0 +1,70 @@
+import torch
+
+def _sample_categorical(categorical_probs):
+  gumbel_norm = (
+    1e-10
+    - (torch.rand_like(categorical_probs) + 1e-10).log())
+  return (categorical_probs / gumbel_norm).argmax(dim=-1)
+
+def _unsqueeze(x, reference):
+  return x.view(
+    * x.shape,
+    * ((1,) * (len(reference.shape) - len(x.shape))))
+
+class AnalyticSampler(torch.nn.Module):
+  def __init__(
+    self,
+    config,
+  )
+    super().__init__()
+    self.config = config
+
+  # abstract methods that must be implemented by child classes
+  def forward(self, x, sigma_t):
+    raise NotImplementedError()
+
+  def noise(self, x, sigma_t):
+    raise NotImplementedError()
+
+  def get_score(self, x, sigma):
+    raise NotImplementedError()
+
+  # also, these attributes must exist:
+  # * self.config
+  # * self.vocab_size
+  # * self.mask_index
+
+  # core sampling logic
+  def _staggered_score(self, score, dsigma):
+    score = score.clone()
+    extra_const = (1 - dsigma.exp()) * score.sum(dim=-1)
+    score *= dsigma.exp()[:, None]
+    score[..., self.mask_index] += extra_const
+    return score
+
+  def _analytic_update(self, x, t, step_size):
+    curr_sigma, _ = self.noise(t)
+    next_sigma, _ = self.noise(t - step_size)
+    dsigma = curr_sigma - next_sigma
+    score = self.get_score(x, curr_sigma)
+    stag_score = self._staggered_score(score, dsigma)
+    probs = stag_score * self._transp_transition(x, dsigma)
+    return _sample_categorical(probs)
+
+  def _denoiser_update(self, x, t):
+    sigma, _ = self.noise(t)
+    score = self.get_score(x, sigma)
+    stag_score = self._staggered_score(score, sigma)
+    probs = stag_score * self._transp_transition(x, sigma)
+    probs[..., self.mask_index] = 0
+    samples = _sample_categorical(probs)
+    return samples
+
+  def _transp_transition(self, i, sigma):
+    sigma = _unsqueeze(sigma, reference=i[..., None])
+    edge = torch.exp(-sigma) * F.one_hot(
+      i, num_classes=self.vocab_size)
+    edge += torch.where(i == self.mask_index,
+                        1 - torch.exp(-sigma).squeeze(-1),
+                        0)[..., None]
+    return edge
---
+++ algorithms/core/sampling/ancestral.py
@@ -0,0 +1,74 @@
+import torch
+
+def _sample_categorical(categorical_probs):
+  gumbel_norm = (
+    1e-10
+    - (torch.rand_like(categorical_probs) + 1e-10).log())
+  return (categorical_probs / gumbel_norm).argmax(dim=-1)
+
+class AncestralSampler(torch.nn.Module):
+  def __init__(
+    self,
+    config,
+  )
+    super().__init__()
+    self.config = config
+
+  # abstract methods that must be implemented by child classes
+  def forward(self, x, sigma_t):
+    raise NotImplementedError
+
+  def noise(self, x, sigma_t):
+    raise NotImplementedError
+
+  # also, these attributes must exist:
+  # * self.config
+  # * self.mask_index
+
+  # core sampling logic
+  def _ddpm_caching_update(self, x, t, dt, p_x0=None):
+    assert self.config.noise.type == 'loglinear'
+    sigma_t, _ = self.noise(t)
+    if t.ndim > 1:
+      t = t.squeeze(-1)
+    assert t.ndim == 1
+    move_chance_t = t[:, None, None]
+    move_chance_s = (t - dt)[:, None, None]
+    assert move_chance_t.ndim == 3, move_chance_t.shape
+    if p_x0 is None:
+      p_x0 = self.forward(x, sigma_t).exp()
+    
+    assert move_chance_t.ndim == p_x0.ndim
+    q_xs = p_x0 * (move_chance_t - move_chance_s)
+    q_xs[:, :, self.mask_index] = move_chance_s[:, :, 0]
+    _x = _sample_categorical(q_xs)
+    
+    copy_flag = (x != self.mask_index).to(x.dtype)
+    return p_x0, copy_flag * x + (1 - copy_flag) * _x
+
+  def _ddpm_update(self, x, t, dt):
+    sigma_t, _ = self.noise(t)
+    sigma_s, _ = self.noise(t - dt)
+    if sigma_t.ndim > 1:
+      sigma_t = sigma_t.squeeze(-1)
+    if sigma_s.ndim > 1:
+      sigma_s = sigma_s.squeeze(-1)
+    assert sigma_t.ndim == 1, sigma_t.shape
+    assert sigma_s.ndim == 1, sigma_s.shape
+    move_chance_t = 1 - torch.exp(-sigma_t)
+    move_chance_s = 1 - torch.exp(-sigma_s)
+    move_chance_t = move_chance_t[:, None, None]
+    move_chance_s = move_chance_s[:, None, None]
+    unet_conditioning = sigma_t
+    log_p_x0 = self.forward(x, unet_conditioning)
+    assert move_chance_t.ndim == log_p_x0.ndim
+    # Technically, this isn't q_xs since there's a division
+    # term that is missing. This division term doesn't affect
+    # the samples.
+    q_xs = log_p_x0.exp() * (move_chance_t
+                             - move_chance_s)
+    q_xs[:, :, self.mask_index] = move_chance_s[:, :, 0]
+    _x = _sample_categorical(q_xs)
+
+    copy_flag = (x != self.mask_index).to(x.dtype)
+    return copy_flag * x + (1 - copy_flag) * _x
