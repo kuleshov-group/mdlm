217f0c488d37f963ab087a9b497e4e4f589ea2f2
Volodymyr Kuleshov <vol.kuleshov@gmail.com>
2024-06-25 17:55:56 -0400

    add diffusion selector

---
+++ algorithms/__init__.py
@@ -0,0 +1,16 @@
+from .sedd import SDD
+from .d3pm import D3PM
+from .mdlm import MDLM
+from .ar import AR
+
+def get_diffusion(config, tokenizer):
+  if config.parameterization == 'sedd':
+    return SEDD(config, tokenizer)
+  elif config.parameterization == 'd3pm':
+    return D3PM(config, tokenizer)
+  elif config.parameterization == 'mdlm':
+    return MDLM(config, tokenizer)
+  elif config.parameterization == 'ar':
+    return AR(config, tokenizer)
+  else:
+    raise NotImplementedError()
